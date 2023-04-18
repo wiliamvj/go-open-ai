007addbbbfc4c4c3f927da4eb9be0f84bb90e9d1
Wiliam V. Joaquim <wiliamjoaquim@gmail.com>
2023-04-18 19:52:57 -0300

    feat: create func for new messages

--- internal/domain/entity/message.go
+++ internal/domain/entity/message.go
@@ -1,3 +1,44 @@
 package entity
 
-type Message struct{}
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+	if m.Content == "" {
+		return errors.New("content is empty")
+	}
+	if m.CreatedAt.IsZero() {
+		return errors.New("invalid created_at")
+	}
+	return nil
+}
