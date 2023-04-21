107998590796fc23fd5517b585b06b3a1e9ae788
Wiliam V. Joaquim <wiliamjoaquim@gmail.com>
2023-04-21 10:55:38 -0300

    feat: init save message in databse

--- internal/infra/repository/chat.go
+++ internal/infra/repository/chat.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
+	"github.com/wiliamvj/go-open-ai/internal/domain/entity"
 	"github.com/wiliamvj/go-open-ai/internal/infra/db"
 )
 
@@ -16,4 +19,49 @@ func NewChatRepositoryMySQL(dbt *sql.DB) *ChatRepositoryMySQL {
 		DB:      dbt,
 		Queries: db.New(dbt),
 	}
+}
+
+func (r *ChatRepositoryMySQL) CreateChat(ctx context.Context, chat *entity.Chat) error {
+	err :=
+		r.Queries.CreateChat(
+			ctx,
+			db.CreateChatParams{
+				ID:               chat.ID,
+				UserID:           chat.UserID,
+				InitialMessageID: chat.InitialSystemMessage.Content,
+				Status:           chat.Status,
+				TokenUsage:       int32(chat.TokensUsage),
+				Model:            chat.Config.Model.Name,
+				ModelMaxTokens:   int32(chat.Config.Model.MaxTokens),
+				Temperature:      float64(chat.Config.Temperature),
+				TopP:             float64(chat.Config.TopP),
+				N:                int32(chat.Config.N),
+				Stop:             chat.Config.Stop[0],
+				MaxTokens:        int32(chat.Config.MaxTokens),
+				PresencePenalty:  float64(chat.Config.PresencePenalty),
+				FrequencyPenalty: float64(chat.Config.FrequencyPenalty),
+				CreatedAt:        time.Now(),
+				UpdatedAt:        time.Now(),
+			},
+		)
+	if err != nil {
+		return err
+	}
+
+	err = r.Queries.AddMessage(
+		ctx,
+		db.AddMessageParams{
+			ID:        chat.InitialSystemMessage.ID,
+			ChatID:    chat.ID,
+			Content:   chat.InitialSystemMessage.Content,
+			Role:      chat.InitialSystemMessage.Role,
+			Tokens:    int32(chat.InitialSystemMessage.Tokens),
+			CreatedAt: chat.InitialSystemMessage.CreatedAt,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
